288468bdcf682508ed92453e9941ece2c4e8d8a3
benjamin-white <benjaminwhite@live.co.uk>
2018-03-30 10:00:18 +0100

    adds node SQL driver, defines initial types, mutations, migrations and models

---
+++ app/Models/data/schema.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const { makeExecutableSchema } = require('graphql-tools')
+const resolvers = require('./resolvers')
+
+// schema definition
+const typeDefs = `
+  type User {
+    id: Int!
+    username: String!
+    email: String!
+    posts: [Post]
+  }
+  type Post {
+    id: Int!
+    title: String!
+    slug: String!
+    content: String!
+    user: User!
+  }
+  type Query {
+    allUsers: [User]
+    allPosts: [Post]
+    fetchUser(id: Int!): User
+    fetchPost(id: Int!): Post
+  }
+  type Mutation {
+    login(email: String!, password: String!): String
+    createUser(username: String!, email: String!, password: String!): User
+    addPost(title: String!, content: String!): Post
+  }
+`
+
+module.exports = makeExecutableSchema({ typeDefs, resolvers })
